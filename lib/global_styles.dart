72271866171cf5342d77ac7086cfa52b0de584fd
Ewen <ewengue1432@gmail.com>
2024-12-27 21:37:57 -0500

    Changed UI of home page

--- lib/global_styles.dart
+++ lib/global_styles.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 const globalStyles = (
   colors: (
-    green: Color(0xFFa7c957),
+    green: Color.fromARGB(255, 102, 222, 22),
     red: Color(0xFFd50000),
+    red2: Color.fromARGB(255, 255, 49, 111),
+    red3: Color.fromARGB(255, 255, 110, 134),
     white: Color(0xFFf0fcff),
     black: Color(0xFF111718),
     primary: Color(0xFF90e0ef),
@@ -11,6 +13,8 @@ const globalStyles = (
     third: Color(0xFF023e8a),
     quart: Color(0xFF03045e),
     accent: Color(0xFFff8443),
+    accent2: Color.fromARGB(255, 243, 207, 88),
+    accent3: Color.fromARGB(255, 243, 184, 88),
   ),
   font: (
     size: 16.0,
--- lib/home.dart
+++ lib/home.dart
@@ -33,22 +33,44 @@ class Home extends StatelessWidget {
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "NanoScience",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                    ),
-                    Text("Level 999")
-                  ],
-                ),
+            child: Container(
+              key: Key('value'),
+              height: double.infinity,
+              color: const Color.fromARGB(224, 171, 227, 255),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                child: LayoutBuilder(builder:
+                    (BuildContext context, BoxConstraints constraints) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: constraints.maxWidth * 0.25,
+                        child: Image.asset(
+                          'assets/images/main_logo.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      Text(
+                        "NanoScience",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22),
+                      ),
+                      Text("Level 999"),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.15,
+                      ),
+                      Text("Total Wins: 999999999"),
+                      Text("Global Rank: #1"),
+                      SizedBox(
+                        height: constraints.maxHeight * 0.15,
+                      ),
+                      ElevatedButton(onPressed: () {}, child: Text("Settings")),
+                    ],
+                  );
+                }),
               ),
             ),
           ),
@@ -68,38 +90,85 @@ class Home extends StatelessWidget {
                       ),
                     ),
                   ),
-                  FilledButton.tonal(
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          globalStyles.colors.green,
+                          const Color.fromARGB(255, 103, 245, 217),
+                          globalStyles.colors.primary,
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: FilledButton.tonal(
                       style: buttonStyle.copyWith(
-                          backgroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.primary),
-                          foregroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.black)),
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.transparent),
+                        foregroundColor:
+                            WidgetStateProperty.all(globalStyles.colors.black),
+                      ),
                       onPressed: () {
                         Navigator.pushNamed(context, '/play');
                       },
-                      child: Text("Play")),
-                  FilledButton.tonal(
+                      child: Text("Play"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          globalStyles.colors.accent2,
+                          globalStyles.colors.accent3,
+                          globalStyles.colors.accent,
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: FilledButton.tonal(
                       style: buttonStyle.copyWith(
-                          backgroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.accent),
-                          foregroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.black)),
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.transparent),
+                        foregroundColor:
+                            WidgetStateProperty.all(globalStyles.colors.black),
+                      ),
                       onPressed: () {
                         Navigator.pushNamed(context, '/leaderboard');
                       },
-                      child: const Text("Leaderboard")),
-                  FilledButton.tonal(
+                      child: const Text("Leaderboard"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          globalStyles.colors.red3,
+                          globalStyles.colors.red2,
+                          globalStyles.colors.red,
+                        ],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: FilledButton.tonal(
                       style: buttonStyle.copyWith(
-                          backgroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.secondary),
-                          foregroundColor: WidgetStateProperty.all(
-                              globalStyles.colors.black)),
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.transparent),
+                        foregroundColor:
+                            WidgetStateProperty.all(globalStyles.colors.black),
+                      ),
                       onPressed: () {},
-                      child: const Text("Shop")),
-                  FilledButton(
-                      style: buttonStyle,
-                      onPressed: null,
-                      child: const Text("Account")),
+                      child: const Text("Shop"),
+                    ),
+                  ),
                   SizedBox(
                     height: 50,
                   )
